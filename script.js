e76791ff28777c01cbf9944a0db1efa91e7acde7
JorgePMC <formalidad.jpg@gmail.com>
2026-09-12 00:40:16 -0300

    se completo la version 1.0 de la pagina, aun le quedan un monton de cosas por pulir, pero, ahora esta mas completa de lo que me pude imaginar. se cambio "registro.htlm" por "inicioSesion.html" por temas de logica(aparentemente no se pueden hacer ambas cosas en una pagina O mi cerebro no daba para mas). se termino la pagina de inicio de sesion y se agrego la pagina de perfil, que solo funciona con las credenciales "UsuarioGenerico" y "12345678" ahora los botones de favoritos son funcionales!!! solo se pueden activar si se inicio sesion correctamente. desde el perfil se da un pequeño resumen y datos que vienen directamente desde java script si uno entra a la pagina sola, esta va a estar vacia.
    y si no me equivoco complete todos los parametros que pedia la rubrica.
    
    voy a dormir...

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         alert(`¡${producto.titulo} fue añadido al carrito!`);
     }
     //Argumentar que carrito sera los elementos llamados por la id
-    const contenedorCarrito = document.getElementById('lista-carrito');
+    const contenedorCarrito = document.getElementById('galeria-carrito');
     if (contenedorCarrito) {
         mostrarCarrito(contenedorCarrito);
     }
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         contenedor.innerHTML = ''; 
 
         if (carrito.length === 0) {
-            contenedor.innerHTML = '<p class="mensajeSinResultados">El carrito está vacío...</p>';
+            contenedor.innerHTML = '<h6 class="mensajeSinResultados">El carrito está vacío...</h6>';
             return;
         }
 
@@ -48,40 +48,187 @@ document.addEventListener('DOMContentLoaded', () => {
             contenedor.appendChild(div);
         });
     }
+    //////////////BUSCADOR/////////////////////
+    const inputBusqueda = document.getElementById('taskInput');
+    const productos = document.querySelectorAll('.producto');
+    const mensajeSinResultados = document.getElementById('sinResultados');
+
+    if (inputBusqueda) {
+        inputBusqueda.addEventListener('input', () => {
+            const textoBusqueda = inputBusqueda.value.toLowerCase().trim();
+            let coincidencias = 0;
+            productos.forEach(producto => {
+                const titulo = producto.querySelector('h3').textContent.toLowerCase();
+                if (titulo.includes(textoBusqueda)) {
+                    producto.style.display = 'flex';
+                    coincidencias++;
+                } else {
+                    producto.style.display = 'none';
+                }
+            });
+
+            if (mensajeSinResultados) {
+                if (coincidencias === 0) {
+                    mensajeSinResultados.style.display = 'block';
+                } else {
+                    mensajeSinResultados.style.display = 'none';
+                }
+            }
+        });
+    }
+    ///////////////logeo//////////////////
+    const formLogin = document.getElementById('formLogin');
+    if (formLogin) {
+        formLogin.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const inputUsuarioElemento = document.getElementById('inputUsuario');
+            const inputPasswordElemento = document.getElementById('inputPassword');
+
+            const inputUsuario = inputUsuarioElemento.value.trim();
+            const inputPassword = inputPasswordElemento.value.trim();
+
+            const errorUsuario = document.getElementById('errorUsuario');
+            const errorPassword = document.getElementById('errorPassword');
+            const errorGeneral = document.getElementById('errorGeneral');
+
+            if (errorUsuario) errorUsuario.textContent = '';
+            if (errorPassword) errorPassword.textContent = '';
+            if (errorGeneral) errorGeneral.textContent = '';
+
+            let hayError = false;
+
+            if (inputUsuario === '') {
+                if (errorUsuario) errorUsuario.textContent = 'Debe ingresar el usuario.';
+                hayError = true;
+            }
+
+            if (inputPassword === '') {
+                if (errorPassword) errorPassword.textContent = 'Debe ingresar la contraseña.';
+                hayError = true;
+            }
+
+            if (hayError) return;
+            
+            if (inputUsuario === 'UsuarioGenerico' && inputPassword === '12345678') {
+                const usuarioActivo = {
+                    nombre: inputUsuario,
+                    foto: 'img/micelaneos/Momonga.png'
+                };
+                localStorage.setItem('usuarioActivo', JSON.stringify(usuarioActivo));
+                window.location.href = 'perfil.html';
+            } else {
+                inputUsuarioElemento.value = '';
+                inputPasswordElemento.value = '';
+
+                if (inputUsuario !== 'UsuarioGenerico') {
+                    if (errorUsuario) errorUsuario.textContent = 'Usuario desconocido.';
+                }
+
+                if (inputPassword !== '12345678') {
+                    if (errorPassword) errorPassword.textContent = 'Contraseña erronea.';
+                }
+            }
+        });
+    }
+    ///////////Cargar perfil con datos//////////////
+    const contenedorPerfil = document.getElementById('info-usuario');
+    const contenedorFavoritos = document.getElementById('galeria-favoritos');
+
+    if (contenedorPerfil) {
+        const usuarioActivo = JSON.parse(localStorage.getItem('usuarioActivo'));
+
+        if (!usuarioActivo) {
+            window.location.href = 'inicioSesion.html';
+        } else {
+            contenedorPerfil.innerHTML = `
+                <img src="${usuarioActivo.foto}" alt="Avatar" class="img-perfil">
+                <h3>¡Hola, ${usuarioActivo.nombre}!</h3>
+                <p>Aqui podras administrar tus favoritos.</p>
+                <button class="btn-logout" onclick="cerrarSesion()">Cerrar Sesión</button>
+            `;
+        }
+    }
+    if (contenedorFavoritos) {
+        mostrarFavoritos(contenedorFavoritos);
+    }
+    //////////añadir favoritos///////////
+    const botonesFavorito = document.querySelectorAll('.btn-favorito');
+    botonesFavorito.forEach(boton => {
+        boton.addEventListener('click', (e) => {
+            const usuarioActivo = JSON.parse(localStorage.getItem('usuarioActivo'));
+
+            if (!usuarioActivo) {
+                alert('Debes iniciar sesión para guardar productos en tus favoritos.');
+                window.location.href = 'inicioSesion.html';
+                return;
+            }
+
+            const tarjeta = e.target.closest('.producto');
+            const producto = {
+                titulo: tarjeta.querySelector('h3').innerText,
+                precio: tarjeta.querySelectorAll('p')[1].innerText,
+                imagen: tarjeta.querySelector('img').src
+            };
+            agregarAFavoritos(producto);
+        });
+    });
 });
-//funcion para eliminar
+//funcion para eliminar del carrito
 function eliminarProducto(index) {
     let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
     carrito.splice(index, 1);
     localStorage.setItem('carrito', JSON.stringify(carrito));
     location.reload();
 }
-    //////////////BUSCADOR/////////////////////
-const inputBusqueda = document.getElementById('taskInput');
-const productos = document.querySelectorAll('.producto');
-const mensajeSinResultados = document.getElementById('sinResultados');
+    ////////////funciones de el perfil////////////////////
+function eliminarFavorito(index) {
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    favoritos.splice(index, 1);
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    location.reload();
+}
 
-if (inputBusqueda) {
-    inputBusqueda.addEventListener('input', () => {
-        const textoBusqueda = inputBusqueda.value.toLowerCase().trim();
-        let coincidencias = 0;
-        productos.forEach(producto => {
-            const titulo = producto.querySelector('h3').textContent.toLowerCase();
-            if (titulo.includes(textoBusqueda)) {
-                producto.style.display = 'flex';
-                coincidencias++;
-            } else {
-                producto.style.display = 'none';
-            }
-        });
+function cerrarSesion() {
+    localStorage.removeItem('usuarioActivo');
+    alert("Has cerrado sesión.");
+    window.location.href = 'index.html';
+}
 
-        if (mensajeSinResultados) {
-            if (coincidencias === 0) {
-                mensajeSinResultados.style.display = 'block';
-            } else {
-                mensajeSinResultados.style.display = 'none';
-            }
-        }
+function mostrarFavoritos(contenedor) {
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    contenedor.innerHTML = '';
+
+    if (favoritos.length === 0) {
+        contenedor.innerHTML = '<h6 class="mensajeSinResultados">Aún no tienes productos guardados en favoritos 💔.</h6>';
+        return;
+    }
+
+    favoritos.forEach((item, index) => {
+        const div = document.createElement('div');
+        div.classList.add('producto-carrito');
+        div.innerHTML = `
+            <img src="${item.imagen}" alt="${item.titulo}" class="img-producto-carrito">
+            <div class="info-producto-carrito">
+                <h3>${item.titulo}</h3>
+                <p>${item.precio}</p>
+            </div>
+            <button class="btn-eliminar" onclick="eliminarFavorito(${index})">Quitar de Favoritos</button>`;
+        contenedor.appendChild(div);
     });
 }
-    /////////////////////////////////
+/////////agregar a favoritos 2////////////
+function agregarAFavoritos(producto) {
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+
+    // Comprobar si el producto ya fue guardado previamente
+    const existe = favoritos.some(fav => fav.titulo === producto.titulo);
+    if (existe) {
+        alert('Este producto ya está en tus favoritos.');
+        return;
+    }
+
+    favoritos.push(producto);
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    alert(`¡${producto.titulo} fue añadido a tus favoritos! ❤️`);
+}
